6f2bc05e328dc2c5c14ce4561e24df9eebb30c8b
Ashfaque Alam <alamashfaquea44@gmail.com>
2026-01-30 23:41:34 +0530

    lecture -60

--- express_1/index.js
+++ express_1/index.js
@@ -1,6 +1,20 @@
 const express=require("express")
 const app=express()
-
+const morgan = require('morgan');
+// lec 59 now we want to creat middleware in order to do this 
+app.use(express.json())
+app.use(morgan('dev'))
+app.use((req,res,next)=>
+{
+    console.log("hello from middle ware")
+    next()
+})
+// now i want to add in request at what time this request come
+app.use((req,res,next)=>
+{
+    req.requestTime=new Date().toISOString()
+    next()
+})
 
 const port=3000
 app.get('/',(req,res)=>{
@@ -11,11 +25,15 @@ app.get("/home",(req,res)=>{
     res.status(200).send("good boy and bad boy")
 })
 
+// If because of some reason some middle where terminat that request next
+// middle where not be called because that cycle will already been finshed
+
 const post_tours=(req,res)=>
 {
     console.log(req.params)
     id=Number(req.params.id)
     console.log(id)
+    console.log(req.requestTime)
     if(id==5)
         res.status(404).json({status:"404 not found",tours:"fails"})
     else
