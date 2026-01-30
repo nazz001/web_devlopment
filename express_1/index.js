0d22d47941844da1e5b0d206c7397f2abae0659e
Ashfaque Alam <alamashfaquea44@gmail.com>
2026-01-30 23:21:17 +0530

    Lecture 54 55 56 done

--- express_1/index.js
+++ express_1/index.js
@@ -1,6 +1,7 @@
 const express=require("express")
 const app=express()
 
+
 const port=3000
 app.get('/',(req,res)=>{
     res.json({message:"welcome to the devlopment"})
@@ -8,6 +9,19 @@ app.get('/',(req,res)=>{
 
 app.get("/home",(req,res)=>{
     res.status(200).send("good boy and bad boy")
+})
+// // Lec 54 creating parameter
+app.post("/home/:id?",(req,res)=>
+{
+    console.log(req.params)
+    id=Number(req.params.id)
+    console.log(id)
+    if(id==5)
+        res.status(404).json({status:"404 not found",tours:"fails"})
+    else
+        res.status(200).json({status:"success",data:5})
+
+    // res.status(302).send("good bod and bad boy")
 })
 
 app.listen(port,()=>{
